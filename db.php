80c5ba0ebc34be760a2f89103d85ee12ff8ee0bd
Neeraj <deyneeraj666@gmail.com>
2021-08-10 20:26:48 +0530

    final Version

---
+++ db.php
@@ -0,0 +1,20 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "3522663_ecommerce";
+
+// Create connection
+$con = mysqli_connect($servername, $username, $password,$db);
+
+// Check connection
+if (!$con) {
+   die("Connection failed: " . mysqli_connect_error());
+}
+
+
+
+
+
+?>
